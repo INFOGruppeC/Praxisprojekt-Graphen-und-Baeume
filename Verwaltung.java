31e53ffb4a2677973c19580525436cce9328b6b1
Lukas Krome <lukaskrome@gmail.com>
2023-02-12 17:47:18 +0100

    push
    Co-authored-by: alexanderboric <alexanderboric@users.noreply.github.com>

--- Verwaltung.java
+++ Verwaltung.java
@@ -10,9 +10,15 @@ public class Verwaltung {
         Graph graph = new Graph ();
         Netzwerkgerät meineGeräte [] = new Netzwerkgerät [10];
         // Netzwerkgerät 1  
-        meineGeräte [0] = new Netzwerkgerät("Lukas-Mac", null, "192.168.1.1", "Apfle") ;
-        meineGeräte [0] = new Netzwerkgerät("Alex-Mac", null, "192.168.1.1", "Apfle") ;
-        meineGeräte [0] = new Netzwerkgerät("Wardemanns-Backdoor", null, "192.168.1.1", "Apfle") ;
+        meineGeräte [0] = new Netzwerkgerät("Lukas-Mac", "00:01:04:05:06:07" , "192.168.1.12", "Apfle") ;
+        meineGeräte [1] = new Netzwerkgerät("Alex-Mac", "AA:BB:CC:DD:EE:FF", "192.168.1.24", "Apfle") ;
+        meineGeräte [2] = new Netzwerkgerät("Wardemanns-Backdoor", "1d:c0:08:bf:8f:65", "8.8.8.8", "Windows") ;
         //lukas
+
+        System.out.println("Schommal was von Kopfhörern gehört?");
+
     }
-}  
+    public static void main(String[] args) {
+        Verwaltung meineVerwaltung = new Verwaltung();
+    }
+}
