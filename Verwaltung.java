b86da052237161aee53c726e5f8590fcb02d19de
alexanderboric <alexanderboric04@gmail.com>
2023-02-16 12:55:42 +0100

    Co-authored-by: l.krome@lift-service.de <l.krome@lift-service.de> Co-authored-by: FloatDev <TheRealFloatDev@users.noreply.github.com>

--- Verwaltung.java
+++ Verwaltung.java
@@ -1,21 +1,42 @@
 // Praxisprojekt zu nicht linearen Datenstrukturen 
 
-
 public class Verwaltung {
-    /* Funktionen 
-     * 1. Abfrage nach gewünstem Algorithmus über einen Binärbaum 
+    /*
+     * Funktionen
+     * 1. Abfrage nach gewünstem Algorithmus über einen Binärbaum
      */
-    public Verwaltung(){
+    public Verwaltung() {
         // Konstruktor
-        Graph graph = new Graph ();
-        Netzwerkgerät meineGeräte [] = new Netzwerkgerät [10];
-        // Netzwerkgerät 1  
-        meineGeräte [0] = new Netzwerkgerät("Lukas-Mac", "00:22:3c:45:86", "192.168.1.1", "Apfle") ;
-        meineGeräte [1] = new Netzwerkgerät("Alex-Mac", null, "192.168.1.2", "Apfle") ;
-        meineGeräte [2] = new Netzwerkgerät("Wardemanns-Backdoor", null, "192.168.1.3", "Microsoft") ;
-        meineGeräte [3] = new Netzwerkgerät("Bastis-Thinkpad", null, "192.168.1.4", "Lenovo") ;
-        meineGeräte [4] = new Netzwerkgerät("Lennarts-Surface", null, "192.168.1.5", "Minimalsoft") ;
-        //lukas
+        Graph graph = new Graph();
+        // Netzwerkgeräte
+        // Insert new network devices with this format: {"hostname": "vendor"}
+        // IP and MAC address are generated randomly / iterative
+        String devices[][] = {
+                { "Lukas-Mac", "Apfle" },
+                { "Alex-Mac", "Apfle" },
+                { "Wardemanns-Backdoor", "Microsoft" },
+                { "Bastis-Thinkpad", "Lenovo" },
+                { "Lennarts-Surface", "Minimalsoft" }
+        };
+        System.out.println(devices.length);
+        for (int i = 0; i < devices.length; i++) {
+            graph.addVertex(new Netzwerkgerät(devices[i][0], generateRandomMac(), "192.168.1." + (i + 1), devices[i][1]));
+        }
+
+        BinaryConverter converter = new BinaryConverter();
+        converter.convertStringToBinary("Bababui");
 
     }
-}  
+
+    private String generateRandomMac() {
+        String mac = "";
+        for (int i = 0; i < 6; i++) {
+            mac += Integer.toHexString((int) (Math.random() * 16));
+        }
+        return mac;
+    }
+
+    public static void main(String[] args) {
+        new Verwaltung();
+    }
+}
