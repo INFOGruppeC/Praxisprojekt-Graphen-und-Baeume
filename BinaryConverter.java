26a8b8d8f9932c782ffecf0e231f96b6185c42a1
alexanderboric <alexanderboric04@gmail.com>
2023-02-23 14:07:45 +0100

    Co-authored-by: FloatDev <TheRealFloatDev@users.noreply.github.com> Co-authored-by: l.krome@lift-service.de <l.krome@lift-service.de>

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -1,3 +1,10 @@
+import java.util.Map;
+import stopwatch;
+import java.security.Key;
+
+import abiturklassen.BinarySearchTree;
+import abiturklassen.BinaryTree;
+
 public class BinaryConverter {
     public void convertStringToBinary(String pEingabe) {
         // Konstruktor
@@ -14,6 +21,37 @@ public class BinaryConverter {
         System.out.println(binary);
 
     }
-    public BinaryConverter
+    BinarySearchTree<Zeichenkette> tree = new BinarySearchTree<Zeichenkette>();
+    public void convertBinaryToString(String pEingabe) {
+        // Konstruktor
+        // split the pEingabe into 8 bit blocks which arent seperated by spaces
+        String[] blocks = pEingabe.split(" ");
+        String binary = "";
+
+       for(String block : blocks){
+              binary = block;
+              
+       }
+k
+    }
+
+    
+
+    private BinaryTree<String> createTree() {
+        return new BinaryTree<String>("", createNode(true, 0, ""), createNode(false, 0, ""));
+    }
+
+    private BinaryTree<String> createNode(boolean isLeft, int depth, String binaryContent) {
+        if (depth == 8) {
+            char c = (char) Integer.parseInt(binaryContent, 2);
+            return new BinaryTree<String>(String.valueOf(c));
+        } else {
+            BinaryTree<String> left = createNode(true, depth + 1, binaryContent + "0");
+            BinaryTree<String> right = createNode(false, depth + 1, binaryContent + "1");
+            return new BinaryTree<String>(isLeft ? "0" : "1", left, right);
+        }
+        
+    }
+
 
 }
